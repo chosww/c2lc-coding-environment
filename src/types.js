d3a9b199a38673bc9270fcdb5346f767cc6ebd4a
Daniel Cho <daniel@Daniels-MacBook-Pro.local>
2019-11-12 15:29:27 -0500

    Add new type for SelectedCommand, which will be changed

--- src/types.js
+++ src/types.js
@@ -5,3 +5,5 @@ export type DeviceConnectionStatus = 'notConnected' | 'connecting' | 'connected'
 export type Program = Array<string>;
 
 export type EditorMode = 'text' | 'block';
+
+export type SelectedCommand = null | { command : 'forward' | 'left' | 'right' } | { func : 'add' | 'delete' };
