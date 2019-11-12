1220244e868f8a01f5e96a33d85bce4e3d1614d3
Daniel Cho <daniel@Daniels-MacBook-Pro.local>
2019-11-12 15:28:01 -0500

    Modify handle function for custom command block

--- src/ProgramBlockEditor.js
+++ src/ProgramBlockEditor.js
@@ -1,8 +1,8 @@
 // @flow
 
-import { Button, Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
 import * as ProgramUtils from './ProgramUtils';
-import type {Program} from './types';
+import type {Program, SelectedCommand} from './types';
 import React from 'react';
 import arrowLeft from 'material-design-icons/navigation/svg/production/ic_arrow_back_48px.svg';
 import arrowRight from 'material-design-icons/navigation/svg/production/ic_arrow_forward_48px.svg';
@@ -13,7 +13,8 @@ import emptyBlockIcon from 'material-design-icons/toggle/svg/production/ic_check
 
 type ProgramBlockEditorProps = {
     program: Program,
-    selectedCommand: string,
+    selectedCommand: SelectedCommand,
+    onFunction: (func: ?string) => void,
     onChange: (Program) => void
 };
 
@@ -28,39 +29,70 @@ export default class ProgramBlockEditor extends React.Component<ProgramBlockEdit
         super(props);
         this.state = {
             addBlockActive: false,
-            deleteBlockActive: false
+            deleteBlockActive: false,
+            customProgramPosition: {}
         }
     }
 
     handleClickAdd = () => {
-        this.setState((state) => {
-            return {
-                addBlockActive: !this.state.addBlockActive,
-                deleteBlockActive: false
-            }
-        });
+        this.props.onFunction(this.props.selectedCommand ?
+            this.props.selectedCommand.func === 'add' ? null : 'add' :
+            'add'
+        );
     };
 
     handleClickDelete = () => {
+        this.props.onFunction(this.props.selectedCommand ?
+            this.props.selectedCommand.func === 'delete' ? null : 'delete' :
+            'delete'
+        );
+    };
+
+    handleClickStep = (index: number) => {
+        if (this.props.selectedCommand) {
+            if (this.props.selectedCommand.func === 'add') {
+                this.props.onChange(ProgramUtils.insert(this.props.program,
+                    index, 'none', 'none'));
+                this.props.onFunction(null);
+            } else if (this.props.selectedCommand.func === 'delete') {
+                this.props.onChange(ProgramUtils.deleteStep(this.props.program, index));
+                this.props.onFunction(null);
+            } else if (this.props.selectedCommand.command) {
+                this.props.onChange(ProgramUtils.overwrite(this.props.program,
+                    index, this.props.selectedCommand.command, 'none'));
+                this.props.onFunction(null);
+            } else if (this.props.selectedCommand.program) {
+                console.log(this.props.savedProgramList[this.props.selectedCommand.program]);
+                this.handleCustomProgramIndex(index, this.props.savedProgramList[this.props.selectedCommand.program].length);
+                this.props.onChange(ProgramUtils.shift(this.props.program,
+                    index, this.props.savedProgramList[this.props.selectedCommand.program], 'none'));
+                this.props.onFunction(null);
+            }
+        }
+    };
+
+    handleCustomProgramIndex = (start: number, programLength: number) => {
+        let currentCustomProgramPositions = this.state.customProgramPosition;
+        const end = start + programLength;
+
+        for(let i=start;i<end;i++) {
+            currentCustomProgramPositions[i] = {
+                name : this.props.selectedCommand.program,
+                start,
+                end
+            }
+        }
+
         this.setState((state) => {
             return {
-                addBlockActive: false,
-                deleteBlockActive: !this.state.deleteBlockActive
+                customProgramPosition : currentCustomProgramPositions
             }
         });
     };
 
-    handleClickStep = (index: number) => {
-        if (this.state.addBlockActive) {
-            this.props.onChange(ProgramUtils.insert(this.props.program,
-                index, 'none', 'none'));
-        } else if (this.state.deleteBlockActive) {
-            this.props.onChange(ProgramUtils.deleteStep(this.props.program, index));
-        } else if (!this.state.addBlockActive && !this.state.deleteBlockActive) {
-            this.props.onChange(ProgramUtils.overwrite(this.props.program,
-                index, this.props.selectedCommand, 'none'));
-        }
-    };
+    componentDidUpdate() {
+        console.log(this.state.customProgramPosition);
+    }
 
     render() {
         return (
@@ -70,7 +102,9 @@ export default class ProgramBlockEditor extends React.Component<ProgramBlockEdit
                         <Button
                             key='addButton'
                             aria-label={this.state.addBlockActive ? 'deactivate add a program to the sequence mode' : 'activate add a program to the sequence mode'}
-                            variant={this.state.addBlockActive ? 'outline-primary' : 'light'}
+                            variant={this.props.selectedCommand ?
+                                this.props.selectedCommand.func === 'add' ? 'outline-primary' : 'light' :
+                                'light'}
                             onClick={this.handleClickAdd}>
                             <Image src={addIcon} />
                         </Button>
@@ -79,7 +113,9 @@ export default class ProgramBlockEditor extends React.Component<ProgramBlockEdit
                         <Button
                             key='deleteButton'
                             aria-label={this.state.deleteBlockActive ? 'deactivate delete a program from the sequence mode' : 'activate delete a program from the sequence mode'}
-                            variant={this.state.deleteBlockActive ? 'outline-primary' : 'light'}
+                            variant={this.props.selectedCommand ?
+                                this.props.selectedCommand.func === 'delete' ? 'outline-primary' : 'light' :
+                                'light'}
                             onClick={this.handleClickDelete}>
                             <Image src={deleteIcon} />
                         </Button>
@@ -93,31 +129,52 @@ export default class ProgramBlockEditor extends React.Component<ProgramBlockEdit
                                     <Row
                                         key={`${programStepNumber}-forward`}
                                         className='justify-content-center'>
-                                        <Button
-                                            aria-label={this.state.addBlockActive ? `Forward button. Press to add an empty command block after this` : this.state.deleteBlockActive ? `Forward button. Press to delete this command` : 'Forward button'}
-                                            onClick={()=>{this.handleClickStep(programStepNumber)}}>
-                                            <Image src={arrowUp} />
+                                        <OverlayTrigger
+                                            key={`${programStepNumber}-forward`}
+                                            placement={'right'}
+                                            overlay={this.state.customProgramPosition[programStepNumber] ? <Tooltip>{this.state.customProgramPosition[programStepNumber].name}</Tooltip> : <Tooltip>Forward</Tooltip> }
+                                        >
+                                            <Button
+                                                variant={this.state.customProgramPosition[programStepNumber] ? 'warning' : 'primary'}
+                                                aria-label={this.state.addBlockActive ? `Forward button. Press to add an empty command block after this` : this.state.deleteBlockActive ? `Forward button. Press to delete this command` : 'Forward button'}
+                                                onClick={()=>{this.handleClickStep(programStepNumber)}}>
+                                                <Image src={arrowUp} />
                                         </Button>
+                                        </OverlayTrigger>
                                     </Row>);
                                 case 'left': return (
                                     <Row
                                         key={`${programStepNumber}-left`}
                                         className='justify-content-center'>
+                                        <OverlayTrigger
+                                            key={`${programStepNumber}-forward`}
+                                            placement={'right'}
+                                            overlay={this.state.customProgramPosition[programStepNumber] ? <Tooltip>{this.state.customProgramPosition[programStepNumber].name}</Tooltip> : <Tooltip>Left</Tooltip> }
+                                        >
                                         <Button
+                                            variant={this.state.customProgramPosition[programStepNumber]? 'warning' : 'primary'}
                                             aria-label={this.state.addBlockActive ? `Left button. Press to add an empty command block after this` : this.state.deleteBlockActive ? `Left button. Press to delete this command` : 'Left button'}
                                             onClick={()=>{this.handleClickStep(programStepNumber)}}>
                                             <Image src={arrowLeft} />
                                         </Button>
+                                        </OverlayTrigger>
                                     </Row>);
                                 case 'right': return (
                                     <Row
                                         key={`${programStepNumber}-right`}
                                         className='justify-content-center'>
+                                        <OverlayTrigger
+                                            key={`${programStepNumber}-forward`}
+                                            placement={'right'}
+                                            overlay={this.state.customProgramPosition[programStepNumber] ? <Tooltip>{this.state.customProgramPosition[programStepNumber].name}</Tooltip> : <Tooltip>Right</Tooltip> }
+                                        >
                                         <Button
+                                            variant={this.state.customProgramPosition[programStepNumber]? 'warning' : 'primary'}
                                             aria-label={this.state.addBlockActive ? `Right button. Press to add an empty command block after this` : this.state.deleteBlockActive ? `Right button. Press to delete this command` : 'Right button'}
                                             onClick={()=>{this.handleClickStep(programStepNumber)}}>
                                             <Image src={arrowRight} />
                                         </Button>
+                                        </OverlayTrigger>
                                     </Row>);
                                 case 'none': return (
                                     <Row
