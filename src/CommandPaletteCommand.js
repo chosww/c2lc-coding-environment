386576122bdfbc40f4237e2fbf22215dac4a6704
Daniel Cho <daniel@Daniels-MacBook-Pro.local>
2019-11-12 15:26:35 -0500

    Add new props and modify handle function for the custom program blocks

--- src/CommandPaletteCommand.js
+++ src/CommandPaletteCommand.js
@@ -1,31 +1,44 @@
 // @flow
 
 import React from 'react';
-import { Button, Col, Image } from 'react-bootstrap';
+import type {SelectedCommand} from './types';
+import { Button, Col, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 type CommandPaletteCommandProps = {
-    commandName: string, 
+    type: string,
+    commandName: string,
     icon: any,
-    selectedCommandName: string,
+    selectedCommandName: SelectedCommand,
     onChange: (commandName: string) => void
 };
 
 export default class CommandPaletteCommand extends React.Component<CommandPaletteCommandProps, {}> {
     handleClick = () => {
-        this.props.onChange(
-            this.props.commandName === this.props.selectedCommandName ? 'none' : this.props.commandName
-        );
+        if (this.props.type === 'movements' && this.props.selectedCommandName !== null ) {
+            this.props.onChange(this.props.selectedCommandName.command === this.props.commandName ? null : this.props.commandName, 'movements');
+        } else if (this.props.type === 'programs' && this.props.selectedCommandName !== null) {
+            this.props.onChange(this.props.selectedCommandName.program === this.props.commandName ? null : this.props.commandName, 'programs');
+        } else {
+            this.props.onChange(this.props.commandName, this.props.type);
+        }
     }
 
     render() {
         return (
             <Col>
-                <Button 
-                    variant={this.props.commandName === this.props.selectedCommandName ? 'outline-primary' : 'light'} 
-                    //aria-label={this.state.selected ? `${} activated` : `${item} inactive`} 
-                    onClick={this.handleClick}>
-                    <Image src={this.props.icon}/>
-                </Button>
+                <OverlayTrigger
+                    placement={'right'}
+                    overlay={<Tooltip>{this.props.commandName}</Tooltip>}
+                >
+                    <Button
+                        variant={this.props.selectedCommandName ?
+                            this.props.selectedCommandName.command === this.props.commandName ? 'outline-primary' : 'light' :
+                            'light'}
+                        //aria-label={this.state.selected ? `${} activated` : `${item} inactive`}
+                        onClick={this.handleClick}>
+                        <Image src={this.props.icon}/>
+                    </Button>
+                </OverlayTrigger>
             </Col>
         )
     }
